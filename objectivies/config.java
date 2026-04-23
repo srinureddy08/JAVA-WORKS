2a1a35fd020c4cafad4cf46996aa8f0e9dfdae46
Srinivasulu Reddy <srinureddy08aa@gmail.com>
2026-04-23 22:34:23 +0530

    Add configuration class for BankingSystem

---
+++ objectivies/config.java
@@ -0,0 +1,14 @@
+class Config {
+    static final String APP_NAME = "BankingSystem";
+    static final int MAX_USERS = 100;
+    static final double VERSION = 1.0;
+}
+public class main6 {
+    public static void main(String[] args) {
+        System.out.println("Application Name: " + Config.APP_NAME);
+        System.out.println("Maximum Users: " + Config.MAX_USERS);
+        System.out.println("Version: " + Config.VERSION);
+
+    }
+}
+
